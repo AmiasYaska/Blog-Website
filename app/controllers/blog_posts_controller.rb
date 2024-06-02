58661ba64c9083c40fba2280e8b6f2c4927623c2
AmiasYaska <amiasyaska@gmail.com>
2024-06-03 00:32:14 +0300

    changes

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -7,8 +7,9 @@ class BlogPostsController < ApplicationController
         @blog_post = BlogPost.find(params[:id])
     rescue ActiveRecord::RecordNotFound 
         redirect_to root_path
-
     end
 
-
+    def new
+    end
+    
 end
