25469a460d93dc651a8c2024bdd06b3f4435facc
AmiasYaska <amiasyaska@gmail.com>
2024-06-03 12:15:28 +0300

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,9 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-  get "/blog_posts/:id", to: "blog_posts#show" , as: :blog_post
   post "/blog_posts", to: "blog_posts#create", as: :blog_posts
 
-
+  get "/blog_posts/:id", to: "blog_posts#show", as: :blog_post
   # Defines the root path route ("/")
   root "blog_posts#index"
 end
